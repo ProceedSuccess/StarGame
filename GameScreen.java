41fdadd01f62418477f95a10ef1655541fc36217
ProceedSuccess <44637885+ProceedSuccess@users.noreply.github.com>
2018-11-07 11:21:50 +0300

    Add files via upload
    Изменил классы МэйнШип и ГеймСкрин

---
+++ GameScreen.java
@@ -0,0 +1,135 @@
+package com.mygdx.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Star2DGame;
+import com.mygdx.game.base.Base2DScreen;
+import com.mygdx.game.math.Rect;
+import com.mygdx.game.pool.BulletPool;
+import com.mygdx.game.sprite.Background;
+import com.mygdx.game.sprite.MainShip;
+import com.mygdx.game.sprite.Star;
+
+
+public class GameScreen extends Base2DScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bgTexture;
+    private Background background;
+
+    private TextureAtlas textureAtlas;
+    private Star[] stars;
+
+    private MainShip mainShip;
+
+    private BulletPool bulletPool;
+
+    private Rect worldbounds;
+
+    Music music;
+
+    @Override
+    public void show() {
+        super.show();
+        this.worldbounds = new Rect();
+        bgTexture = new Texture("bg.png");
+        background = new Background(new TextureRegion(bgTexture));
+        textureAtlas = new TextureAtlas("mainAtlas.tpack");
+        stars =new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(textureAtlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new MainShip(textureAtlas, bulletPool);
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("D:/2/StarGame/android/assets/muzyka-sssr-kosmicheskaya-muzyka.mp3"));
+            music.play();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        checkCollisions();
+        deleteAllDestroyed();
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveObjects(delta);
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void deleteAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveObjects();
+    }
+
+    public void draw() {
+        Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveObjects(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        this.worldbounds = worldBounds;
+        background.resize(worldBounds);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        bgTexture.dispose();
+        textureAtlas.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return super.keyUp(keycode);
+    }
+
+    public boolean touchInLeft(Vector2 touch, int pointer){
+        return touch.x >= worldbounds.getLeft() && touch.x <= worldbounds.getRight()/2 && touch.y >= worldbounds.getBottom() && touch.y <= worldbounds.getTop();
+    }
+    public boolean touchInRight(Vector2 touch, int pointer){
+        return touch.x >= worldbounds.getLeft()/2 && touch.x <= worldbounds.getRight() && touch.y >= worldbounds.getBottom() && touch.y <= worldbounds.getTop();
+    }
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+//        return super.touchDown(touch, pointer);
+        if(touchInLeft(touch,pointer)) mainShip.moveLeft();
+        else if(touchInRight(touch,pointer)) mainShip.moveRight();
+        return false;
+    }
+}
