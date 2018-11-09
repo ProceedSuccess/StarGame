d38f703a8316443ddc5f9fc0d90347e42e8b7b2a
ProceedSuccess <44637885+ProceedSuccess@users.noreply.github.com>
2018-11-09 20:55:44 +0300

    Add files via upload
    В двух классах добавил 8 строчек

---
+++ GameScreen.java
@@ -0,0 +1,150 @@
+package com.mygdx.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Star2DGame;
+import com.mygdx.game.Utils.EnemiesEmmiter;
+import com.mygdx.game.base.Base2DScreen;
+import com.mygdx.game.math.Rect;
+import com.mygdx.game.pool.BulletPool;
+import com.mygdx.game.pool.EnemyPool;
+import com.mygdx.game.sprite.Background;
+import com.mygdx.game.sprite.MainShip;
+import com.mygdx.game.sprite.Star;
+
+public class GameScreen extends Base2DScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bgTexture;
+    private Background background;
+
+    private TextureAtlas textureAtlas;
+    private Star[] stars;
+
+    private MainShip mainShip;
+
+    private BulletPool bulletPool;
+
+    private Sound laserSound;
+    private Sound bulletSound;
+    private Music music;
+
+    private EnemyPool enemyPool;
+    private EnemiesEmmiter enemiesEmmiter;
+
+    @Override
+    public void show() {
+        super.show();
+        laserSound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
+        bulletSound = Gdx.audio.newSound(Gdx.files.internal("sounds/bullet.wav"));
+
+        bgTexture = new Texture("bg.png");
+        background = new Background(new TextureRegion(bgTexture));
+        textureAtlas = new TextureAtlas("mainAtlas.tpack");
+        stars =new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(textureAtlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new MainShip(textureAtlas, bulletPool, laserSound);
+
+        enemyPool = new EnemyPool(bulletPool, worldBounds, bulletSound);
+        enemiesEmmiter = new EnemiesEmmiter(enemyPool, worldBounds, textureAtlas);
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setLooping(true);
+        music.play();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        checkCollisions();
+        deleteAllDestroyed();
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveObjects(delta);
+        enemyPool.updateActiveObjects(delta);
+        enemiesEmmiter.generate(delta);
+    }
+
+    public void checkCollisions() {
+
+    }
+   // добавил enemyPool.freeAllDestroyedActiveObjects();
+    public void deleteAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveObjects();
+        enemyPool.freeAllDestroyedActiveObjects();
+    }
+
+    public void draw() {
+        Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveObjects(batch);
+        enemyPool.drawActiveObjects(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        bgTexture.dispose();
+        textureAtlas.dispose();
+        music.dispose();
+        laserSound.dispose();
+        bulletSound.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return super.keyUp(keycode);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        mainShip.touchDown(touch, pointer);
+        return super.touchDown(touch, pointer);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        mainShip.touchUp(touch, pointer);
+        return super.touchUp(touch, pointer);
+    }
+}
